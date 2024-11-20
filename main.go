522d18edd199d4f628145754222d99718bb055ac
ZakYeo <zakyeomanson@googlemail.com>
2024-11-20 08:22:28 +0000

    Added second unit test to test adding multiple items into cache

--- main.go
+++ main.go
@@ -18,14 +18,20 @@ func main() {
     fmt.Println("Hello, World!") 
 }
 
-func SetCacheItem(item string, cache Cache){
+func AddCacheItem(item string){
   cacheItem:= CacheItem{
     Value: item,
   }
-  currentCache.Items = append(cache.Items, cacheItem)
+  currentCache.Items = append(currentCache.Items, cacheItem)
 }
 
 func GetCache()(*Cache){
     copy := currentCache // Make a shallow copy of the cache
     return &copy         // Return a pointer to the copy
+}
+
+func ClearCache(){
+  currentCache = Cache{
+    Items: []CacheItem{},
+  }
 }
--- main_test.go
+++ main_test.go
@@ -3,34 +3,32 @@ package main
 import "testing"
 
 func TestCanSetCacheWithOneItem(t *testing.T) {
-  testCacheItems:= Cache {
-    Items: []CacheItem{},
-  }
 
-  SetCacheItem("new_cache_item", testCacheItems)
+  AddCacheItem("new_cache_item")
   currentCache := GetCache()
-
 
   if(currentCache.Items[0].Value != "new_cache_item"){
     t.Errorf("Unable to successfully set new_cache_item into cache. Got: %s, Want: %s", currentCache.Items[0].Value, "new_cache_item")
   }
+
+  ClearCache()
 }
 
 
-/*func TestCanSetCacheWithMultipleItems(t *testing.T) {
-  testCacheItems:= Cache {
-    Items: []CacheItem{},
+func TestCanSetCacheWithMultipleItems(t *testing.T) {
+
+  AddCacheItem("new_cache_item")
+  currentCache := GetCache()
+
+  if(currentCache.Items[0].Value != "new_cache_item"){
+    t.Errorf("Unable to successfully set new_cache_item into cache. Got: %s, Want: %s", currentCache.Items[0].Value, "new_cache_item")
   }
 
-  newTestCache := SetCache("new_cache_item", testCacheItems)
-  newTestCache2 := SetCache("new_cache_item2", testCacheItems)
-
-  if(newTestCache.Items[0].Value != "new_cache_item"){
-    t.Errorf("Unable to successfully set new_cache_item into cache. Got: %s, Want: %s", newTestCache.Items[0].Value, "new_cache_item")
-  }
-
+  AddCacheItem("new_cache_item2")
+  currentCache = GetCache()
   
-  if(newTestCache2.Items[1].Value != "new_cache_item2"){
-    t.Errorf("Unable to successfully set new_cache_item into cache. Got: %s, Want: %s", newTestCache.Items[1].Value, "new_cache_item2")
+  if(currentCache.Items[1].Value != "new_cache_item2"){
+    t.Errorf("Unable to successfully set new_cache_item2 into cache. Got: %s, Want: %s", currentCache.Items[1].Value, "new_cache_item2")
   }
-}*/
+  ClearCache()
+}
