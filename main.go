0991ab0b6ba43836c462813ec304edceb175f8a6
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2025-01-15 16:19:14 +0000

    Added timer to allow time to launch shards before launching the server

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
+import (
+	"time"
+)
+
 func main() {
 	go LaunchShard()
 	go LaunchShard()
 	go LaunchShard()
+	time.Sleep(1 * time.Second) // Allow shards to initialize before starting the server
 	LaunchServer()
 }
